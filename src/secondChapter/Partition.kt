c0bd46416ddf8ec71d49404f30cd1c405125de17
angelodias <angelossdias@gmail.com>
2020-07-16 11:57:57 -0300

    Partition.kt makes the list partitioned by a value

---
+++ src/secondChapter/Partition.kt
@@ -0,0 +1,70 @@
+package secondChapter
+
+import Utils.Companion.SinglyLinkedList
+
+class Partition {
+
+    fun partition(linkedList: SinglyLinkedList<Int>, byNumber: Int): SinglyLinkedList<Int> {
+        var node = linkedList.head
+        val iterator = linkedList.iterator()
+        var globalPrevious = node
+        var localPrevious = node
+        var firstNode = true
+        var firstGreaterThanOrEqual = false
+
+        while (iterator.hasNext()) {
+
+            if(!firstNode && node!!.value < byNumber) {
+                localPrevious!!.next = node.next
+
+                if(!firstGreaterThanOrEqual && linkedList.head!!.value >= byNumber) {
+                    firstGreaterThanOrEqual = true
+
+                    node.next = globalPrevious
+                    linkedList.head = node
+                } else {
+                    node.next = globalPrevious!!.next
+                    globalPrevious.next = node
+                }
+
+                globalPrevious = node
+            }
+
+            localPrevious = node
+            node = node!!.next
+            firstNode = false
+
+            iterator.next()
+        }
+
+        return linkedList
+    }
+
+    fun otherPartition(linkedList: SinglyLinkedList<Int>, byNumber: Int): SinglyLinkedList<Int> {
+        if(!linkedList.isEmpty()) {
+            var node = linkedList.head
+            var head = node
+            var tail = node
+
+            while (node != null) {
+                val next = node.next
+
+                if (node.value < byNumber) {
+                    node.next = head
+                    head = node
+                } else {
+                    tail!!.next = node
+                    tail = node
+                }
+
+                node = next
+            }
+
+            tail!!.next = null
+            linkedList.head = head
+        }
+
+        return linkedList
+    }
+
+}
---
+++ src/secondChapter/PartitionTest.kt
@@ -0,0 +1,277 @@
+package secondChapter
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+import Utils.Companion.SinglyLinkedList
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class PartitionTest {
+    private lateinit var partition: Partition
+
+    @BeforeAll
+    fun setup(){
+        partition = Partition()
+    }
+
+    @Test
+    fun partitionTest() {
+        val expected = SinglyLinkedList<Int>(0)
+        val tested = SinglyLinkedList<Int>(0)
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.add(3)
+        expected.add(2)
+        expected.add(1)
+        expected.add(5)
+        expected.add(8)
+        expected.add(5)
+        expected.add(10)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 5).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(12)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(2)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(4)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 2).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(3)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.add(3)
+        expected.add(2)
+        expected.add(1)
+        expected.add(5)
+        expected.add(8)
+        expected.add(5)
+        expected.add(10)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 5).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.partition(tested, 2).toTypedArray()
+            )
+    }
+
+    @Test
+    fun otherPartitionTest() {
+        val expected = SinglyLinkedList<Int>(0)
+        val tested = SinglyLinkedList<Int>(0)
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.add(1)
+        expected.add(2)
+        expected.add(3)
+        expected.add(5)
+        expected.add(8)
+        expected.add(5)
+        expected.add(10)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 5).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.add(1)
+        expected.add(2)
+        expected.add(10)
+        expected.add(5)
+        expected.add(8)
+        expected.add(5)
+        expected.add(3)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(12)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(2)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 12).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(3)
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(10)
+        tested.add(2)
+        tested.add(4)
+
+        expected.addAll(tested)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 2).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        tested.add(5)
+        tested.add(8)
+        tested.add(5)
+        tested.add(3)
+        tested.add(10)
+        tested.add(2)
+        tested.add(1)
+
+        expected.add(1)
+        expected.add(2)
+        expected.add(3)
+        expected.add(5)
+        expected.add(8)
+        expected.add(5)
+        expected.add(10)
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 5).toTypedArray()
+            )
+
+        tested.clear()
+        expected.clear()
+
+        Assertions
+            .assertArrayEquals(
+                expected.toTypedArray(),
+                partition.otherPartition(tested, 2).toTypedArray()
+            )
+    }
+
+}
