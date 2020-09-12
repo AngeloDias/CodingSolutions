9b66385cabdebfe87ed5cbe85a03c181c73772e4
angelodias <angelossdias@gmail.com>
2020-09-12 12:47:01 -0300

    Calculating alternating counters

---
+++ src/sideImplementations/AlternateCountersValues.kt
@@ -0,0 +1,44 @@
+package sideImplementations
+
+class AlternateCountersValues {
+
+    companion object {
+        fun solution(n: Int, a: IntArray): IntArray {
+            val counters = IntArray(n)
+            var maxCounter = 0
+            var isNewMaxCounter = false
+            var setMaxCounter = BooleanArray(n)
+            var previousMaxCounter = maxCounter
+
+            for(i in a.indices) {
+                if(a[i] == n+1 && maxCounter > 0 && isNewMaxCounter) {
+//                    setMaxCounter = BooleanArray(n)
+                    isNewMaxCounter = false
+                    previousMaxCounter = maxCounter
+                }
+
+                if(a[i] in 1..n) {
+                    if(!setMaxCounter[a[i] - 1]) {
+                        setMaxCounter[a[i] - 1] = true
+                        counters[a[i] - 1] = previousMaxCounter
+                    }
+
+                    counters[a[i] - 1]++
+
+                    if(counters[a[i] - 1] > maxCounter) {
+                        isNewMaxCounter = true
+                        maxCounter = counters[a[i] - 1]
+                    }
+                }
+            }
+
+            for(i in setMaxCounter.indices) {
+                if(!setMaxCounter[i]) {
+                    counters[i] = maxCounter
+                }
+            }
+
+            return counters
+        }
+    }
+}
---
+++ src/tests/AlternateCountersValuesTest.kt
@@ -0,0 +1,63 @@
+package tests
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import sideImplementations.AlternateCountersValues
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AlternateCountersValuesTest {
+
+    @Test
+    fun solutionTest() {
+        var array = intArrayOf(3, 4, 4, 6, 1, 4, 4)
+        var expectedArray = intArrayOf(3, 2, 2, 4, 2)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(5, array))
+
+        array = intArrayOf(3, 4, 4, 6, 1, 5, 4)
+        expectedArray = intArrayOf(3, 2, 2, 3, 3)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(5, array))
+
+        array = intArrayOf(1, 2, 2, 2, 1, 2, 2)
+        expectedArray = intArrayOf(2)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(2, 2, 2, 2, 2)
+        expectedArray = intArrayOf(0)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(1)
+        expectedArray = intArrayOf(1)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(2)
+        expectedArray = intArrayOf(0)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(2, 1)
+        expectedArray = intArrayOf(1)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(1, 2)
+        expectedArray = intArrayOf(1)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(1, array))
+
+        array = intArrayOf(1, 2)
+        expectedArray = intArrayOf(1, 1, 0, 0, 0, 0, 0)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(7, array))
+
+        array = intArrayOf(8)
+        expectedArray = intArrayOf(0, 0, 0, 0, 0, 0, 0)
+
+        Assertions.assertArrayEquals(expectedArray, AlternateCountersValues.solution(7, array))
+    }
+}
