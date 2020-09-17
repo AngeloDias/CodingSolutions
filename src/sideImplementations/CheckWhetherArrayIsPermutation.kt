af1c09d21ae53b87b16d6922a2d82dfba57e9058
angelodias <angelossdias@gmail.com>
2020-09-17 12:09:05 -0300

    Changing the type to use Long and HashMap

--- src/sideImplementations/CheckWhetherArrayIsPermutation.kt
+++ src/sideImplementations/CheckWhetherArrayIsPermutation.kt
@@ -6,36 +6,18 @@ class CheckWhetherArrayIsPermutation {
 
     companion object {
         fun solution(a: IntArray): Int {
-            var elementsSum = 0
+            var elementsSum = 0L
             var smallestElement = 1000000001
             var biggestElement = 0
-            var retBitNum = 0UL
-            var mask: ULong
+            val numbersInSequenceMap = HashMap<Int, Int>()
 
             for(i in a.indices) {
                 elementsSum += a[i]
-                mask = 1UL shl (a[i] - 1) // saving number in binary representation
 
-                // checking if the number was already evaluated in binary representation
-                if((retBitNum and mask) == 0UL) {
-                    retBitNum = retBitNum or mask // saving the new number to the bit vector
-                    if(a.size == 100000) {
-                        print("retBitNum attribution: $retBitNum\n")
-                    }
+                if(!numbersInSequenceMap.containsKey(a[i])) {
+                    numbersInSequenceMap[a[i]] = 1
                 } else {
-                    // means that a number is duplicated inside sequence
-                    if(a.size == 100000){
-                        print("Returned inside bitwise\n")
-                        print("retBitNum: $retBitNum\n")
-                        print("a[i]: ${a[i]}\n")
-                        print("i: $i\n")
-                    }
-
-                    retBitNum = retBitNum and mask.inv()
-
-                    if((retBitNum and (retBitNum - 1UL)) != 0UL) {
-                        return 0
-                    }
+                    return 0
                 }
 
                 if (a[i] < smallestElement) {
@@ -48,8 +30,8 @@ class CheckWhetherArrayIsPermutation {
             }
 
             if((biggestElement == 1 && a.size == 1) || a.size > 1
-                && elementsSum == Functions.calculateSequenceSum(smallestElement, a.size).toInt()
-                && retBitNum and 1UL == 1UL) {
+                && elementsSum == Functions.calculateSequenceSum(smallestElement, a.size).toLong()
+                && numbersInSequenceMap.containsKey(1)) {
                 return 1
             }
 
