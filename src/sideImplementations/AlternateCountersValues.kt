76bfe1a21a3cb518c44d5cb07b2ada7edb94058e
angelodias <angelossdias@gmail.com>
2020-10-06 20:32:37 -0300

    Calculating the elements and trying to structure the max count verifying

--- src/sideImplementations/AlternateCountersValues.kt
+++ src/sideImplementations/AlternateCountersValues.kt
@@ -5,43 +5,31 @@ class AlternateCountersValues {
     companion object {
         // avoid to use inner for
         fun solution(n: Int, a: IntArray): IntArray {
-            val counters = IntArray(n)
+            val counters = counting(a, n)
             var maxCounter = 0
-            var isNewMaxCounter = false
-            var setMaxCounter = BooleanArray(n)
-            var previousMaxCounter = maxCounter
-            val countIncrements = IntArray(n)
 
             for(i in a.indices) {
-                if(a[i] == n+1 && maxCounter > 0 && isNewMaxCounter) {
-//                    setMaxCounter = BooleanArray(n)
-                    isNewMaxCounter = false
-                    previousMaxCounter = maxCounter
+                val position = a[i] - 1
+
+                if(counters[position] > maxCounter) {
+                    maxCounter = counters[position]
                 }
 
-                if(a[i] in 1..n) {
-                    if(!setMaxCounter[a[i] - 1]) {
-                        setMaxCounter[a[i] - 1] = true
-                        counters[a[i] - 1] = previousMaxCounter
-                    }
-
-                    counters[a[i] - 1]++
-                    countIncrements[a[i] - 1]++
-
-                    if(counters[a[i] - 1] > maxCounter) {
-                        isNewMaxCounter = true
-                        maxCounter = counters[a[i] - 1]
-                    }
-                }
-            }
-
-            for(i in setMaxCounter.indices) {
-                if(!setMaxCounter[i]) {
-                    counters[i] = maxCounter
+                if(a[i] == n+1) {
                 }
             }
 
             return counters
+        }
+
+        private fun counting(elements: IntArray, withMaximumValue: Int): IntArray {
+            val countElements = IntArray(withMaximumValue + 1)
+
+            for(i in countElements.indices) {
+                countElements[elements[i]]++
+            }
+
+            return countElements
         }
     }
 }
