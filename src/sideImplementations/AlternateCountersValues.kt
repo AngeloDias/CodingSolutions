5c455b8bd2ab2e6c97235ecde1d4c3aa02b00993
angelodias <angelossdias@gmail.com>
2020-10-09 20:01:08 -0300

    Checking when the operations are just to update

--- src/sideImplementations/AlternateCountersValues.kt
+++ src/sideImplementations/AlternateCountersValues.kt
@@ -9,29 +9,33 @@ class AlternateCountersValues {
             var maxCounter = 0
             val theCounters = IntArray(n)
 
-            for(k in localCounters.indices) {
-                val position = a[k] - 1
-            }
+            // this case can happen when the elements in 'a' are just to update the counters values
+            if(localCounters[localCounters.size - 1] > 0) {
 
-            for(i in a.indices) {
-                val position = a[i] - 1
+                for (i in a.indices) {
+                    val position = a[i] - 1
 
-                if(localCounters[position] > maxCounter) {
-                    maxCounter = localCounters[position]
+                    if (localCounters[position] > maxCounter) {
+                        maxCounter = localCounters[position]
+                    }
+
+                    if (a[i] == n + 1) {
+                    }
                 }
 
-                if(a[i] == n+1) {
-                }
             }
 
-            return localCounters
+            return theCounters
         }
 
         private fun counting(elements: IntArray, withMaximumValue: Int): IntArray {
-            val countElements = IntArray(withMaximumValue + 1)
+            val countElements = IntArray(withMaximumValue + 2)
 
-            for(i in countElements.indices) {
+            for(i in 0 until countElements.size - 1) {
                 countElements[elements[i]]++
+
+                // using the last position to save the sum of the values of the array
+                countElements[countElements.size - 1] += countElements[elements[i]]
             }
 
             return countElements
